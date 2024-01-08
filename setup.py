18b8983e99fa48ab975a0b9f63ba320e6130033c
= <=>
2024-01-08 12:31:05 +0100

    add requeirements.txt

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__="0.0.0"
+
+REPO_NAME = "End-to-end-ML-with-mlflow"
+AUTHOR_USER_NAME = "hsiebenou"
+SRC_REPO = "mlproject"
+AUTHOR_EMAIL = "hermann.siebenou@hotmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small pythoh package for ml app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https:github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Trackter": f"https:github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues"
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
